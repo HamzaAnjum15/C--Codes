15e47e199b37ff0c4609074eb12a22d0e7803ba3
Hamza Anjum <66873696+HamzaAnjum15@users.noreply.github.com>
2022-11-10 22:25:53 +0500

    Add files via upload

---
+++ Sum of array/sumofarray.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+// class Ar
+// {
+//     int a[5] = {1, 2, 3, 4, 5};
+//     int sum()
+//     {
+//         int t=0;
+//         for (int i = 0; i < 5; i++)
+//         {
+            
+//             t=t+a[i];
+            
+//         }
+//         cout<<t;
+//     };
+// };
+int main()
+{
+    int a[5] = {1, 2, 3, 4, 5};
+    
+        int t=0;
+        for (int i = 0; i < 5; i++)
+        {
+            
+            t=t+a[i];
+            
+        }
+        cout<<t;
+    
+   
+
+
+    return 0;
+}
